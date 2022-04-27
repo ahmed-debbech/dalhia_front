e6dd74dd98f7ea549e3f23fc50979d7c3c3186dc
ahmed-debbech <debbech.ahmed@gmail.com>
2022-04-27 04:50:35 +0100

    add topic component

---
+++ src/app/models/Topic.ts
@@ -0,0 +1,12 @@
+export class Topic{
+    id: number;
+    banned: boolean;
+    datePublished: string;
+    dateRemoved : string;
+    //tag : string;
+    text : string;
+    title : string;
+    score : number;
+    lastBeTopicOfTheDay : string;
+    //user : user;
+}
