b8612d6aa026d01b38d3919242a550e35881db8f
AzizBoughattas <boughattas.medaziz@esprit.tn>
2022-05-01 20:20:34 +0100

    fixed CRUD User

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,7 +1,9 @@
 import { HttpResponse } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
-import { AbstractControl, FormControl, FormGroup, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, NgForm, ValidationErrors, ValidatorFn, Validators } from '@angular/forms';
 import { User } from 'app/models/user';
+import * as moment from 'moment';
+import { Subscription } from 'rxjs';
 import { UserService } from './user.service';
 
 
@@ -10,12 +12,16 @@ import { UserService } from './user.service';
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit  {
 
 users :User[];
-user : User 
+user : User ;
+
 signupForm : FormGroup
 disabled=true;
+editMode = false;
+birthDate : any
+subscription : Subscription;
 
 authRequest: any = {
   "email" : "boughattasaziz@gmail.csom",
@@ -26,25 +32,45 @@ authRequest: any = {
 
   ngOnInit(): void {
     //this.getAccessToken(this.authRequest)
-    this.service.getUserss().subscribe(user => this.users=user);
+    this.service.getUserss().subscribe(user => {
+      this.users=user
+      }
+    );
+   
     this.signupForm = new FormGroup({
       'email' : new FormControl(null, [Validators.required , Validators.email]),
       'first_name' : new FormControl(null, [Validators.required]),
       'last_name' : new FormControl(null, [Validators.required]),
       'password' : new FormControl(null, [Validators.required , Validators.minLength(8)]),
-      'phone' : new FormControl(null, [Validators.required ,Validators.maxLength(8),Validators.minLength(8)]),
-      'date_birth' : new FormControl(null, [Validators.required]),
+      'phone' : new FormControl(null, [Validators.required ,Validators.pattern("^[0-9]{8}$")]),
+      'date_birth' : new FormControl(null, [Validators.required,this.dateValidator]),
       'city' : new FormControl(null, [Validators.required]),
       'state' : new FormControl(null, [Validators.required]),
       'zipCode' : new FormControl(null, [Validators.required,Validators.maxLength(4)]),
       'address' : new FormControl(null, [Validators.required]),
+      'userId' : new FormControl(null),
     })
   }
 
+  unsubscribe() {
+    this.editMode=false;
+    this.disabled=true
+    this.ngOnInit();
+  }
+
+ 
 
   onSubmit() {
-    this.service.addUser(this.signupForm.value).subscribe();
-    this.signupForm.reset();
+    
+    if (this.editMode){
+      console.log('hello')
+      console.log(this.signupForm.value)
+      this.service.updateUser(this.user.userId,this.signupForm.value).subscribe();
+      this.signupForm.reset();
+    }else {
+   this.service.addUser(this.signupForm.value).subscribe();
+   this.signupForm.reset();
+    }
   }
 
   onDelete(id:string) {
@@ -52,22 +78,22 @@ authRequest: any = {
   }
 
   onUpdate(id : string) {
+    this.editMode=true
     this.disabled=false;
-    this.service.getById(id).subscribe(u =>{ this.user=u
-      
+   this.subscription= this.service.getById(id).subscribe(u =>{ this.user=u
       this.signupForm=new FormGroup({
         'email' : new FormControl(this.user?.email, [Validators.required , Validators.email]),
         'first_name' : new FormControl(this.user?.first_name, [Validators.required]),
         'last_name' : new FormControl(this.user?.last_name, [Validators.required]),
-        'password' : new FormControl(null, [Validators.required , Validators.minLength(8)]),
         
-        'phone' : new FormControl(this.user?.phone, [Validators.required ,Validators.maxLength(8),Validators.minLength(8)]),
-        'date_birth' : new FormControl(this.user?.date_birth, [Validators.required]),
+        'phone' : new FormControl(this.user?.phone, [Validators.required ,Validators.pattern("^[0-9]{8}$")]),
+        'date_birth' : new FormControl(this.user?.date_birth , [Validators.required,this.dateValidator]),
         'city' : new FormControl(this.user?.city, [Validators.required]),
         'state' : new FormControl(this.user?.state, [Validators.required]),
         'zipCode' : new FormControl(this.user?.zipCode, [Validators.required,Validators.maxLength(4)]),
         'address' : new FormControl(this.user?.address, [Validators.required]),
       })})
+     
   }
 
   // getAccessToken(authRequest) {
@@ -78,4 +104,23 @@ authRequest: any = {
   //     }
   //   )
   // }
+
+  onEditItem(){
+
+  }
+  //chouf fazet date
+
+  dateValidator(control: FormControl): { [s: string]: boolean } {
+    if (control.value) {
+      console.log(control.value);
+      const date = moment(control.value);
+      const today = moment();
+      if (date.isAfter(today)&&(date.isValid)) {
+        return { 'invalidDate': true }
+      }
+    }
+    return null;
+  }
+
 }
+
