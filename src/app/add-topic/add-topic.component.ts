2450b0aba385e658f71d96281cc9c958ffa47661
ahmed-debbech <debbech.ahmed@gmail.com>
2022-05-06 14:33:27 +0100

    fix feed and add form topic

--- src/app/add-topic/add-topic.component.ts
+++ src/app/add-topic/add-topic.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Topic } from 'app/models/Topic';
+import { TopicService } from 'app/services/topic.service';
 
 @Component({
   selector: 'add-topic',
@@ -7,8 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddTopicComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router : Router, private ts : TopicService) { }
 
   ngOnInit(): void {
+  }
+
+  add(tit : string, tex : string){
+    console.log(tit + tex);
+    var top  = new Topic();
+      top.title = tit
+      top.text = tex
+      top.tag = "DISCUSSION"   
+      this.ts.addNewTopic(top).subscribe(res => {console.log(res)})
+      //this.router.navigate(['feed']);
   }
 }
--- src/app/services/topic.service.ts
+++ src/app/services/topic.service.ts
@@ -9,22 +9,25 @@ import { Observable } from 'rxjs';
 export class TopicService {
 
 
-  baseUrl : string = "http://localhost:8089/api/v1/forum/topics/"
+  baseUrl : string = "http://localhost:8089/api/v1/forum/topics"
   constructor(private http : HttpClient) { }
 
+  addNewTopic(topic : Topic) : Observable<Topic>{
+    return this.http.post<Topic>(this.baseUrl, topic);
+  }
   getAllTopics() : Observable<Topic[]>{
     return this.http.get<Topic[]>(this.baseUrl+"")
   }
   getTopicById(id : string) : Observable<Topic> {
-    return this.http.get<Topic>(this.baseUrl+ id);
+    return this.http.get<Topic>(this.baseUrl+ "/" + id);
   }
   banTopic(id : string) : Observable<any>{
-    return this.http.put<any>(this.baseUrl + id + "/bans/true", {});
+    return this.http.put<any>(this.baseUrl + "/" + id + "/bans/true", {});
   }
   unbanTopic(id : string) : Observable<any>{
-    return this.http.put<any>(this.baseUrl + id + "/bans/false", {});
+    return this.http.put<any>(this.baseUrl + "/" + id + "/bans/false", {});
   }
   removeTopic(id: string) : Observable<any> {
-    return this.http.delete<any>(this.baseUrl + id)
+    return this.http.delete<any>(this.baseUrl + "/" + id)
   }
 }
